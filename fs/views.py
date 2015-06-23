69437178260e738244029ec4da850cb1c64db687
igor <shevchenkcoigor@gmail.com>
2015-06-23 22:56:06 +0300

    fix bag contenttype

--- fs/views.py
+++ fs/views.py
@@ -13,7 +13,7 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         qs_item = Item.objects.filter(enable=1).select_related('main_group')
         context['items_popular'] = qs_item.order_by('-popular')[:8]
-        c_type = ContentType.objects.get_for_model(Item)
+        c_type = ContentType.objects.get(app_label="materials", model="item")
         context['items_comment'] = qs_item.prefetch_related('comments', 'comments__user').\
         annotate(count_comment=Count('comments')).filter(count_comment__gte=2, comments__content_type=c_type)[:16]
         context['items_new'] = qs_item.select_related('creator', 'main_group').order_by('-date_create')[:20]
