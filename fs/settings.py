ee9bca664e7de8a1b8c0b603760922af7fa81dce
igor <shevchenkcoigor@gmail.com>
2015-07-10 14:35:33 +0300

    heroku

--- fs/settings.py
+++ fs/settings.py
@@ -156,3 +156,8 @@ TEMPLATE_DIRS = (
 )
 
 DEFAULT_FROM_EMAIL = 'shevchenkcoigor@gmail.com'
+
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
