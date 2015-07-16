ef844ba6f9656b84040844b328f91f9f7cdf364f
igor <shevchenkcoigor@gmail.com>
2015-07-16 11:50:03 +0300

    fix bag static files (collectstatic)

--- fs/settings.py
+++ fs/settings.py
@@ -143,8 +143,8 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATIC_URL = '/static_root/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
 MEDIA_URL = "/media/"
 
