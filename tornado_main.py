16ff7abe9cf5483d92e65bb9d4b00402ba61bb59
igor <shevchenkcoigor@gmail.com>
2015-07-23 14:58:30 +0300

    tornado_main fix bag load settings

--- tornado_main.py
+++ tornado_main.py
@@ -7,12 +7,11 @@
 # http://localhost:8080/hello-django
 
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Activate your virtual env
 activate_env = os.path.expanduser(os.path.join(BASE_DIR, "ENV/bin/activate_this.py"))
 execfile(activate_env, dict(__file__=activate_env))
-
-from fs.settings import BASE_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fs.settings")
 
