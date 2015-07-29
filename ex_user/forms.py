80ff384a5f536c8234324b92e7bfba4c3baeec1e
igor <shevchenkcoigor@gmail.com>
2015-07-29 16:32:37 +0300

    add help_text registration form

--- ex_user/forms.py
+++ ex_user/forms.py
@@ -4,6 +4,7 @@ from django import forms
 from django.contrib.auth.models import User
 from djangular.styling.bootstrap3.forms import Bootstrap3Form
 from djangular.forms import NgFormValidationMixin, NgModelForm
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserMeta(type(NgModelForm), type(Bootstrap3Form)):
@@ -24,6 +25,15 @@ class UserForm(NgModelForm, NgFormValidationMixin, Bootstrap3Form):
     __metaclass__ = UserMeta
     form_name = 'user_form'
     scope_prefix = 'user_model'
+
+    def __init__(self, **kwargs):
+        super(UserForm, self).__init__(**kwargs)
+        self.fields['password'].help_text = ''
+
+        for name, type_field in self.fields.items():
+            for error, message in type_field.get_potential_errors():
+                if message.capitalize() and not self.fields[name].help_text:
+                    self.fields[name].help_text += message.capitalize()
 
     class Meta:
         model = User
