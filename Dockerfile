2ac1f0cc3c0c733ebdd6c9b20921bd87db04ef3e
igor <shevchenkcoigor@gmail.com>
2019-02-15 17:59:18 +0200

    add Dockerfile, docker-compose

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:2.7
+ENV PYTHONUNBUFFERED 1
+RUN apt-get update -y
+# RUN apt-get install -y python-pip python-dev build-essential libpq-dev zip unzip libmysqlclient-dev tzdata rsync ssh
+RUN apt-get install -y binutils libproj-dev gdal-bin
+RUN mkdir /app
+WORKDIR /app
+COPY requirements.txt /app
+RUN pip install -r requirements.txt
+COPY . /app/
+# ENV TZ=Europe/Kiev
+# RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- fs/settings.py
+++ fs/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -56,9 +55,12 @@ INSTALLED_APPS = (
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
+        'ENGINE':
+        'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL':
+        'http://127.0.0.1:9200/',
+        'INDEX_NAME':
+        'haystack',
     },
 }
 
@@ -81,14 +83,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'fs.urls'
 
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
-    )
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,  'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,9 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fs',
         'USER': 'fs',
-        'PASSWORD': '1',
-        'HOST': '',
-        'POST': '',
+        'PASSWORD': 'fs',
+        'HOST': 'db',
+        'PORT': '',
     }
 }
 
@@ -145,17 +145,13 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static_root/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 DEFAULT_FROM_EMAIL = 'shevchenkcoigor@gmail.com'
