31a7c3e2f47ccb4b3bcc7d064580f3d891147ee0
igor <shevchenkcoigor@gmail.com>
2019-02-20 10:00:21 +0200

    clear entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-python manage.py makemigrations --noinput && python manage.py migrate --noinput
+# python manage.py makemigrations --noinput && python manage.py migrate --noinput
 
-python manage.py loaddata dumpdata/db.json
+# python manage.py loaddata dumpdata/db.json
 
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
-python manage.py rebuild_index --noinput
+# python manage.py rebuild_index --noinput
 
-gunicorn fs.wsgi --log-file -
+# gunicorn fs.wsgi --log-file -
