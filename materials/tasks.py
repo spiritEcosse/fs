26734eb88903db67a18ac8ea827a5130833c25f3
igor <shevchenkcoigor@gmail.com>
2019-02-25 18:00:29 +0200

    replace kinopoik url on yandex.net

--- materials/tasks.py
+++ materials/tasks.py
@@ -18,7 +18,7 @@ def parser_premiere():
     name_group = 'Video'
     group, __ = Group.objects.get_or_create(
         title=name_group, slug=slug(name_group))
-
+    URL = 'https://st.kp.yandex.net/images/film_iphone/iphone360_'
     # gs = goslate.Goslate()
     url = os.environ.get('RSS_URL')
     items = feedparser.parse(url)
@@ -31,8 +31,7 @@ def parser_premiere():
             title=title,
             # origin_title=gs.translate(title, 'en'),
             creator=user,
-            original_image='https://st.kp.yandex.net/images/film_iphone/' +
-            item.links[1].href.split('/')[-1],
+            original_image=URL + item.links[1].href.split('/')[-1],
             tags=[],
             main_group=group,
             description=item.summary,
